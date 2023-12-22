51d7f0e4c7b0f1de1d45feec7dee578eb900c9da
Ashish Prajapati <ashish.prajapati@yoti.com>
2023-12-22 16:59:34 +0530

    localhost javascript practice folder added

---
+++ javascript_practice/hello.js
@@ -0,0 +1,110 @@
+//comment in javascript
+console.log("hello") //to print
+
+let fname = "ashish" // let is a latest keyword used to declare variable
+// javacscript too a dynamically typed language just like python
+var lname = "prajapati" // var is used in ES6
+
+fullname = fname +' '+ lname //string concatenation works similar as python
+console.log("To print use", fullname, "either use ", `{fname}`) //check once again
+
+console.log(2/0) // infinity 
+
+let str_name = "amit" //string
+let num = 99999999999999999999999999999 //number datatype
+let float = 0.000000001 //number datatype
+let bool = true // boolean datatype
+
+console.log(typeof(str_name))
+console.log(typeof(num))
+console.log(typeof(float))
+console.log(typeof(bool))
+
+let fullstring = "This little guiding light of mine, i'm going to let it shine"
+console.log(fullstring.toUpperCase())
+console.log(fullstring.toLowerCase())
+console.log(fullstring.slice(3,10)) // 's littl'
+console.log(fullstring.indexOf('i', 7)) //14
+
+fruits = ["apple", 'orange', 'banana']
+fruits.push("watermelon")
+console.log(fruits) // ['apple', 'orange', 'banana', 'watermelon']
+fruits.unshift("muskmelon")
+console.log(fruits) // ['muskmelon', 'apple', 'orange', 'banana', 'watermelon']
+console.log(fruits.length)
+fruits.pop()
+console.log(fruits) // ['muskmelon', 'apple', 'orange', 'banana']
+fruits.shift()
+console.log(fruits) // ['apple', 'orange', 'banana']
+console.log(fruits[4])
+console.log(fruits[-1]) //banana
+console.log(fruits[0]) //apple
+
+let arr = new Array("Old", 'method', 'to', 'create', 'array', 'in', 'javascript')
+console.log(arr)
+
+let profile = {
+    "name": "ashish prajapati",
+    "age": 28,
+    "profession": "NOC Engineer"
+}
+
+console.log(profile) // just like dictionary in python
+console.log(profile.name)
+console.log(profile.age)
+console.log(profile.profession)
+profile["address"] = "AECS Layout"
+console.log(profile)
+console.log(typeof(profile))
+
+age = 18
+
+if (age === 18) {
+    console.log("You just turned 18, just take a sip!!!")
+} else if (age < 18) {
+    console.log("You are not authorized to drink.")
+} else if (age > 65) {
+    console.log("You are going to die")
+} else {
+    console.log("You are eligible to drink")
+}
+
+
+// let n = 5
+// for (i=0; i<n; i++) {
+//     console.log(i+1)
+// }
+
+// let i = 5; star='*'
+// for (j=0; j<i; j++) {
+//     console.log(star.repeat(j+1))
+// }
+
+// let i = 8; star='*'
+// while (i > 5) {
+//     console.log(i, star.repeat(i))
+//     i--
+// }
+
+fruits = ['apple', 'banana', 'orange', 'grapes', 'litchi']
+console.log(fruits)
+console.log(fruits.length)
+
+for (i=0; i < fruits.length; i++) {
+    console.log(fruits[i])
+}
+
+let numb = 10, star='*';
+for(i=1; i<=numb; i++) {
+    console.log(star.repeat(i))
+}
+
+veges = ['spinach','onion', 'carrot', 'patato', 'cauliflower', 'cabbage']
+
+for (i=0; i<veges.length; i++) {
+    console.log(veges[i])
+}
+
+let dict = {'fruit':'apple', 'veges':'onion', 'beverages':'pastry', 'lunch':'mash-potato', 'dinner':'rice'}
+console.log(dict)
+console.log(dict.keys)
